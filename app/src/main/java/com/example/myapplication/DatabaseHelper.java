4b1c637a25f556f0fab8caf05a434e1069ebc2c0
raja <digisailordev@gmail.com>
2020-01-27 08:18:58 +0530

    gh

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -31,5 +32,17 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(db);
 
+    }
+    public boolean insertData(String name,String surname,String marks){
+        SQLiteDatabase db=this.getWritableDatabase();
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(COL_2,name);
+        contentValues.put(COL_3,surname);
+        contentValues.put(COL_4,marks);
+        long result=db.insert(TABLE_NAME,null,contentValues);
+        if (result== -1)
+            return false;
+        else
+            return true;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatabaseHelper myDb;
+    EditText editName,editsurname,editMarks;
+    Button btnAddData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb=new DatabaseHelper(this);
+        editName=(EditText)findViewById(R.id.editText);
+        editsurname=(EditText)findViewById(R.id.editText2);
+        editMarks=(EditText)findViewById(R.id.editText3);
+        btnAddData=(Button) findViewById(R.id.button);
+
     }
 }
