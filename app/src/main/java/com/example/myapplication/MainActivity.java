fd9dfa2ce9fdfa0e34c919b797ab13bb0931367c
raja <digisailordev@gmail.com>
2020-01-27 08:25:56 +0530

    gh

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDb;
@@ -19,6 +21,20 @@ public class MainActivity extends AppCompatActivity {
         editsurname=(EditText)findViewById(R.id.editText2);
         editMarks=(EditText)findViewById(R.id.editText3);
         btnAddData=(Button) findViewById(R.id.button);
+        Adddata();
 
+    }
+    public void Adddata()
+    {
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               boolean isInserted= myDb.insertData(editName.getText().toString(),editsurname.getText().toString(),editMarks.getText().toString());
+             if(isInserted=true)
+                 Toast.makeText(MainActivity.this,"data Inserted",Toast.LENGTH_LONG).show();
+             else
+                 Toast.makeText(MainActivity.this,"data not Inserted",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
